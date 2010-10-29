7e25443505b3e8011f99bb94bb768a4d9c16cd46
Ben Langfeld <ben@langfeld.me>
2010-10-29 13:38:00 +0100

    Patch to Strophe.js
    * Documented here: http://github.com/metajack/strophejs/pull/7

--- Resources/Strophe/strophe.js
+++ Resources/Strophe/strophe.js
@@ -2762,6 +2762,9 @@ Strophe.Connection.prototype = {
         var typ = elem.getAttribute("type");
         var cond, conflict;
         if (typ !== null && typ == "terminate") {
+            if (this.disconnecting) {
+                return;
+            }
             // an error occurred
             cond = elem.getAttribute("condition");
             conflict = elem.getElementsByTagName("conflict");
